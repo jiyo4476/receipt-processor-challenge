23dc9d8d77dacf9e38ecd1d7a433a0607e1805ff
Jimmy Young <jimmy.young4476@gmail.com>
2024-12-17 13:43:51 -0700

    Refactor for Receipt Functions
    + Moved functions getPointsAlphanumerical, getPointsRoundAmount, getPointsMultipleOf25, getPointsForItemNum, getPointsForItems, getPointsForOddDate, getPointsForTimeOfPurchase
    to Receipt object
    
    + Updated test cases to test in package functions

--- models/receipt.go
+++ models/receipt.go
@@ -47,20 +47,20 @@ func (r Receipt) ValidateTotal() error {
 func (r Receipt) Points() (int64, error) {
 	points := int64(0)
 	// One point for every alphanumerical character in the retailer name
-	points += getNumAlphanumerical(r.Retailer)
+	points += r.getPointsAlphanumerical()
 
 	// 50 points if the total is a round dollar amount with no cents
-	points += getPointsRoundAmount(r.Total)
+	points += r.getPointsRoundAmount()
 
 	// 25 points if total is a multiple of .25
-	points += getPointsMultipleOf25(r.Total)
+	points += r.getPointsMultipleOf25()
 
 	// 5 points for every two items on the receipt
 	points += r.getPointsForItemNum()
 
 	// if trimmed length of item description is a multiple of 3, multiply price by
 	// 0.2 and round up to the nearest int. The result is the number of points added
-	itemPoints, err := getPointsForItems(r.Items)
+	itemPoints, err := r.getPointsForItems()
 	if err != nil {
 		// return error
 		return -1, err
@@ -69,17 +69,17 @@ func (r Receipt) Points() (int64, error) {
 	points += itemPoints
 
 	// 6 points in the day in the purchsae date is odd
-	points += getPointsForOddDate(r.PurchaseDate)
+	points += r.getPointsForOddDate()
 
 	// 10 points if the time of purchase is after 2pm but before 4pm
-	points += getPointsForTimeOfPurchase(r.PurchaseTime)
+	points += r.getPointsForTimeOfPurchase()
 
 	return points, err
 }
 
-func getNumAlphanumerical(s string) int64 {
+func (r Receipt) getPointsAlphanumerical() int64 {
 	count := int64(0)
-	for _, c := range s {
+	for _, c := range r.Retailer {
 		if unicode.IsLetter(c) || unicode.IsNumber(c) {
 			count++
 		}
@@ -87,30 +87,30 @@ func getNumAlphanumerical(s string) int64 {
 	return count
 }
 
-func getPointsRoundAmount(amount string) int64 {
-	if roundAmountRegex.MatchString(amount) {
+func (r Receipt) getPointsRoundAmount() int64 {
+	if roundAmountRegex.MatchString(r.Total) {
 		return 50
 	}
 	return 0
 }
 
-func getPointsMultipleOf25(amount string) int64 {
-	if multipleOf25Regex.MatchString(amount) {
+func (r Receipt) getPointsMultipleOf25() int64 {
+	if multipleOf25Regex.MatchString(r.Total) {
 		return 25
 	}
 	return 0
 }
 
-func getPointsForOddDate(date string) int64 {
-	match := oddRegex.MatchString(date)
+func (r Receipt) getPointsForOddDate() int64 {
+	match := oddRegex.MatchString(r.PurchaseDate)
 	if match {
 		return 6
 	}
 	return 0
 }
 
-func getPointsForTimeOfPurchase(purchaseTime string) int64 {
-	if matched := timeOfPurchaseRegex.MatchString(purchaseTime); matched {
+func (r Receipt) getPointsForTimeOfPurchase() int64 {
+	if matched := timeOfPurchaseRegex.MatchString(r.PurchaseTime); matched {
 		return 10
 	}
 	return 0
@@ -120,9 +120,9 @@ func (r Receipt) getPointsForItemNum() int64 {
 	return int64((len(r.Items) / 2) * 5)
 }
 
-func getPointsForItems(items []Item) (int64, error) {
+func (r Receipt) getPointsForItems() (int64, error) {
 	points := int64(0)
-	for _, curr_item := range items {
+	for _, curr_item := range r.Items {
 		if len(strings.Trim(curr_item.ShortDescription, " "))%3 == 0 {
 			i, err := strconv.ParseFloat(curr_item.Price, 64)
 			if err != nil {
--- models/receipt_test.go
+++ models/receipt_test.go
@@ -6,71 +6,263 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGetNumAlphanumerical(t *testing.T) {
-	value := getNumAlphanumerical("")
+func createRetailerTestReceipt(name string) Receipt {
+	receipt := Receipt{
+		Retailer:     name,
+		PurchaseDate: "2022-01-01",
+		PurchaseTime: "13:01",
+		Items: []Item{
+			{ShortDescription: "Item01", Price: "10.00"},
+			{ShortDescription: "Item02", Price: "10.00"},
+		},
+		Total: "20.00",
+	}
+	return receipt
+}
+
+func TestGetNumAlphanumerical_NoChars(t *testing.T) {
+	receipt := createRetailerTestReceipt("")
+	value := receipt.getPointsAlphanumerical()
 	assert.Equal(t, int64(0), value)
-	value = getNumAlphanumerical("a")
+}
+
+func TestGetNumAlphanumerical_OneChar(t *testing.T) {
+	receipt := createRetailerTestReceipt("a")
+	value := receipt.getPointsAlphanumerical()
 	assert.Equal(t, int64(1), value)
-	value = getNumAlphanumerical("hello123world")
+}
+func TestGetNumAlphanumerical_Valid01(t *testing.T) {
+	receipt := createRetailerTestReceipt("hello123world")
+	value := receipt.getPointsAlphanumerical()
 	assert.Equal(t, int64(13), value)
-	value = getNumAlphanumerical("ABCDEF12345")
+}
+func TestGetNumAlphanumerical_Valid02(t *testing.T) {
+	receipt := createRetailerTestReceipt("ABCDEF12345")
+	value := receipt.getPointsAlphanumerical()
 	assert.Equal(t, int64(11), value)
-	value = getNumAlphanumerical("!@#$%^&*")
+}
+func TestGetNumAlphanumerical_Valid03(t *testing.T) {
+	receipt := createRetailerTestReceipt(" hello world ")
+	value := receipt.getPointsAlphanumerical()
+	assert.Equal(t, int64(10), value)
+}
+func TestGetNumAlphanumerical_Valid04(t *testing.T) {
+	receipt := createRetailerTestReceipt("h3110,w0r1d!")
+	value := receipt.getPointsAlphanumerical()
+	assert.Equal(t, int64(10), value)
+}
+func TestGetNumAlphanumerical_Invalid(t *testing.T) {
+	receipt := createRetailerTestReceipt("!@#$%^&*")
+	value := receipt.getPointsAlphanumerical()
 	assert.Equal(t, int64(0), value)
-	value = getNumAlphanumerical(" hello world ")
-	assert.Equal(t, int64(10), value)
-	value = getNumAlphanumerical("h3110,w0r1d!")
-	assert.Equal(t, int64(10), value)
-
 }
 
-func TestGetPointsRoundAmount(t *testing.T) {
-	value := getPointsRoundAmount("00.00")
+func TestGetPointsRoundAmount_Valid(t *testing.T) {
+	receipt := Receipt{
+		Retailer:     "Test Retailer",
+		PurchaseDate: "2022-01-01",
+		PurchaseTime: "13:01",
+		Items:        []Item{},
+		Total:        "00.00",
+	}
+	value := receipt.getPointsRoundAmount()
 	assert.Equal(t, int64(50), value)
-	value = getPointsRoundAmount("00.59")
+}
+
+func TestGetPointsRoundAmount_NoPoints(t *testing.T) {
+	receipt := Receipt{
+		Retailer:     "Test Retailer",
+		PurchaseDate: "2022-01-01",
+		PurchaseTime: "13:01",
+		Items: []Item{
+			{ShortDescription: "Item01", Price: "0.50"},
+			{ShortDescription: "Item02", Price: "0.09"},
+		},
+		Total: "00.59",
+	}
+	value := receipt.getPointsRoundAmount()
 	assert.Equal(t, int64(0), value)
 }
 
-func TestGetPointsMultipleOf25(t *testing.T) {
-	value := getPointsMultipleOf25("10.00")
+func TestGetPointsMultipleOf25_00(t *testing.T) {
+	receipt := Receipt{
+		Retailer:     "Test Retailer",
+		PurchaseDate: "2022-01-01",
+		PurchaseTime: "13:01",
+		Items: []Item{
+			{ShortDescription: "Item01", Price: "5.00"},
+			{ShortDescription: "Item02", Price: "5.00"},
+		},
+		Total: "10.00",
+	}
+	value := receipt.getPointsMultipleOf25()
 	assert.Equal(t, int64(25), value)
-	value = getPointsMultipleOf25("00.25")
+}
+
+func TestGetPointsMultipleOf25_25(t *testing.T) {
+	receipt := Receipt{
+		Retailer:     "Test Retailer",
+		PurchaseDate: "2022-01-01",
+		PurchaseTime: "13:01",
+		Items: []Item{
+			{ShortDescription: "Item01", Price: "5.00"},
+			{ShortDescription: "Item02", Price: "5.25"},
+		},
+		Total: "10.25",
+	}
+	value := receipt.getPointsMultipleOf25()
 	assert.Equal(t, int64(25), value)
-	value = getPointsMultipleOf25("00.50")
+}
+
+func TestGetPointsMultipleOf25_50(t *testing.T) {
+	receipt := Receipt{
+		Retailer:     "Test Retailer",
+		PurchaseDate: "2022-01-01",
+		PurchaseTime: "13:01",
+		Items: []Item{
+			{ShortDescription: "Item01", Price: "0.25"},
+			{ShortDescription: "Item02", Price: "0.25"},
+		},
+		Total: "00.50",
+	}
+	value := receipt.getPointsMultipleOf25()
 	assert.Equal(t, int64(25), value)
-	value = getPointsMultipleOf25("00.75")
+}
+
+func TestGetPointsMultipleOf25_75(t *testing.T) {
+	receipt := Receipt{
+		Retailer:     "Test Retailer",
+		PurchaseDate: "2022-01-01",
+		PurchaseTime: "13:01",
+		Items: []Item{
+			{ShortDescription: "Item01", Price: "0.50"},
+			{ShortDescription: "Item02", Price: "0.25"},
+		},
+		Total: "00.75",
+	}
+	value := receipt.getPointsMultipleOf25()
 	assert.Equal(t, int64(25), value)
-	value = getPointsMultipleOf25("00.99")
+}
+
+func TestGetPointsMultipleOf25_NoPoints(t *testing.T) {
+	receipt := Receipt{
+		Retailer:     "Test Retailer",
+		PurchaseDate: "2022-01-01",
+		PurchaseTime: "13:01",
+		Items: []Item{
+			{ShortDescription: "Item01", Price: "0.50"},
+			{ShortDescription: "Item02", Price: "0.49"},
+		},
+		Total: "00.99",
+	}
+	value := receipt.getPointsMultipleOf25()
 	assert.Equal(t, int64(0), value)
 }
 
-func TestGetPointsForOddDate(t *testing.T) {
-	value := getPointsForOddDate("2022-01-01")
+func TestGetPointsForOddDate_Odd(t *testing.T) {
+	receipt := Receipt{
+		Retailer:     "Test Retailer",
+		PurchaseDate: "2022-01-01",
+		PurchaseTime: "13:01",
+		Items: []Item{
+			{ShortDescription: "Item01", Price: "0.50"},
+			{ShortDescription: "Item02", Price: "0.49"},
+		},
+		Total: "00.99",
+	}
+	value := receipt.getPointsForOddDate()
 	assert.Equal(t, int64(6), value)
-	value = getPointsForOddDate("2022-01-02")
+}
+
+func TestGetPointsForOddDate_Even(t *testing.T) {
+	receipt := Receipt{
+		Retailer:     "Test Retailer",
+		PurchaseDate: "2022-01-02",
+		PurchaseTime: "13:01",
+		Items: []Item{
+			{ShortDescription: "Item01", Price: "0.50"},
+			{ShortDescription: "Item02", Price: "0.49"},
+		},
+		Total: "00.99",
+	}
+	value := receipt.getPointsForOddDate()
 	assert.Equal(t, int64(0), value)
 }
 
-func TestGetPointsForTimeOfPurchase(t *testing.T) {
-	value := getPointsForTimeOfPurchase("14:00")
-	assert.Equal(t, int64(10), value)
-	value = getPointsForTimeOfPurchase("15:00")
-	assert.Equal(t, int64(10), value)
-	value = getPointsForTimeOfPurchase("13:00")
+func TestGetPointsForTimeOfPurchase_1PM(t *testing.T) {
+	receipt := Receipt{
+		Retailer:     "Test Retailer",
+		PurchaseDate: "2022-01-02",
+		PurchaseTime: "13:00",
+		Items: []Item{
+			{ShortDescription: "Item01", Price: "0.50"},
+			{ShortDescription: "Item02", Price: "0.49"},
+		},
+		Total: "00.99",
+	}
+	value := receipt.getPointsForTimeOfPurchase()
 	assert.Equal(t, int64(0), value)
-	value = getPointsForTimeOfPurchase("16:00")
+}
+
+func TestGetPointsForTimeOfPurchase_2PM(t *testing.T) {
+	receipt := Receipt{
+		Retailer:     "Test Retailer",
+		PurchaseDate: "2022-01-02",
+		PurchaseTime: "14:00",
+		Items: []Item{
+			{ShortDescription: "Item01", Price: "0.50"},
+			{ShortDescription: "Item02", Price: "0.49"},
+		},
+		Total: "00.99",
+	}
+	value := receipt.getPointsForTimeOfPurchase()
+	assert.Equal(t, int64(10), value)
+}
+func TestGetPointsForTimeOfPurchase_3PM(t *testing.T) {
+	receipt := Receipt{
+		Retailer:     "Test Retailer",
+		PurchaseDate: "2022-01-02",
+		PurchaseTime: "15:00",
+		Items: []Item{
+			{ShortDescription: "Item01", Price: "0.50"},
+			{ShortDescription: "Item02", Price: "0.49"},
+		},
+		Total: "00.99",
+	}
+	value := receipt.getPointsForTimeOfPurchase()
+	assert.Equal(t, int64(10), value)
+}
+func TestGetPointsForTimeOfPurchase_4PM(t *testing.T) {
+	receipt := Receipt{
+		Retailer:     "Test Retailer",
+		PurchaseDate: "2022-01-02",
+		PurchaseTime: "16:00",
+		Items: []Item{
+			{ShortDescription: "Item01", Price: "0.50"},
+			{ShortDescription: "Item02", Price: "0.49"},
+		},
+		Total: "00.99",
+	}
+	value := receipt.getPointsForTimeOfPurchase()
 	assert.Equal(t, int64(0), value)
 }
 
 func TestGetPointsForItems(t *testing.T) {
-	items := []Item{
-		{ShortDescription: "Mountain Dew 12PK", Price: "6.49"},
-		{ShortDescription: "Emils Cheese Pizza", Price: "12.25"},
-		{ShortDescription: "Knorr Creamy Chicken", Price: "1.26"},
-		{ShortDescription: "Doritos Nacho Cheese", Price: "3.35"},
-		{ShortDescription: "   Klarbrunn 12-PK 12 FL OZ  ", Price: "12.00"},
+	receipt := Receipt{
+		Retailer:     "Test Retailer",
+		PurchaseDate: "2022-01-01",
+		PurchaseTime: "13:01",
+		Items: []Item{
+			{ShortDescription: "Mountain Dew 12PK", Price: "6.49"},
+			{ShortDescription: "Emils Cheese Pizza", Price: "12.25"},
+			{ShortDescription: "Knorr Creamy Chicken", Price: "1.26"},
+			{ShortDescription: "Doritos Nacho Cheese", Price: "3.35"},
+			{ShortDescription: "   Klarbrunn 12-PK 12 FL OZ  ", Price: "12.00"},
+		},
+		Total: "35.35",
 	}
-	value, err := getPointsForItems(items)
+
+	value, err := receipt.getPointsForItems()
 	if err != nil {
 		t.Fatalf("Error calculating points for items: %v", err)
 	}
